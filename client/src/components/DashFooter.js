fcb4cefd32f5f8d7c4f33066a3ff3866fc7f58b6
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-29 11:59:50 -0700

    Added DashFooter component and integrate it into DashLayout

---
+++ client/src/components/DashFooter.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
+import { faHouse } from "@fortawesome/free-solid-svg-icons"; // Import specific icon
+import { useNavigate, useLocation } from "react-router-dom"; // Import hooks for navigation and location
+import { GoHome } from "react-icons/go";
+
+const DashFooter = () => {
+  // DashFooter component
+  const navigate = useNavigate(); // Hook to navigate programmatically
+  const { pathname } = useLocation(); // Hook to get the current path
+  const onGoHomeClicked = () => navigate("/dash"); // Function to navigate to the home path
+
+  let goHomeButton = null; // Initialize goHomeButton as null
+  if (pathname !== "/dash") {
+    // Check if the current path is not the home path
+    goHomeButton = (
+      <button
+        className="dash-footer_buton icon-button"
+        title="Home"
+        onClick={onGoHomeClicked}
+      >
+        <FontAwesomeIcon icon={faHouse} />
+      </button>
+    );
+  }
+
+  const content = (
+    <footer className="dash-footer">
+      {goHomeButton}
+      <p>Current User:</p>
+      <p>Status: </p>
+    </footer>
+  );
+  return content;
+};
+export default DashFooter;
--- client/src/components/DashLayout.js
+++ client/src/components/DashLayout.js
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import DashHeader from "./DashHeader";
+import DashFooter from "./DashFooter";
+
 const DashLayout = () => {
   return (
     <>
@@ -7,6 +9,7 @@ const DashLayout = () => {
       <div className="dash-container">
         <Outlet />
       </div>
+      <DashFooter />
     </>
   );
 };
