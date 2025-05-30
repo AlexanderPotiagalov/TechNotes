3353f0b4326830b35d0cd36230222bc9f360cfc3
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-30 15:37:13 -0700

    Imported PersistentLogin into App and nested it within the structure.

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
 import Prefetch from "./features/auth/Prefetch";
+import PersistLogin from "./features/auth/PersistLogin";
 
 function App() {
   return (
@@ -18,22 +19,26 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="/*" element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path="new" element={<NewNote />} />
-              <Route path=":id" element={<EditNote />} />
-            </Route>
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path="new" element={<NewUserForm />} />
-              <Route path=":id" element={<EditUser />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
+
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
+
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
             </Route>
-          </Route>{" "}
-          {/* End Dash */}
+            {/* End Dash */}
+          </Route>
         </Route>
       </Route>
     </Routes>
