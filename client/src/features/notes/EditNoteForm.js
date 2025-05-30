cf11e756552229306f66b687fe980c79a325a88b
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-29 18:16:07 -0700

    Added NewNoteForm for creating notse and EditNoteForm for editing existing notes.

---
+++ client/src/features/notes/EditNoteForm.js
@@ -0,0 +1,204 @@
+import { useState, useEffect } from "react";
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+// EditNoteForm component for editing an existing note
+const EditNoteForm = ({ note, users }) => {
+  // Mutation hooks for updating a note
+  const [updateNote, { isLoading, isSuccess, isError, error }] =
+    useUpdateNoteMutation();
+
+  // Mutation hooks for deleting a note
+  const [
+    deleteNote,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteNoteMutation();
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(note.title);
+  const [text, setText] = useState(note.text);
+  const [completed, setCompleted] = useState(note.completed);
+  const [userId, setUserId] = useState(note.user);
+
+  // Validate title and text using regex
+  useEffect(() => {
+    if (isSuccess || isDelSuccess) {
+      setTitle("");
+      setText("");
+      setUserId("");
+      navigate("/dash/notes");
+    }
+  }, [isSuccess, isDelSuccess, navigate]);
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onTextChanged = (e) => setText(e.target.value);
+  const onCompletedChanged = (e) => setCompleted((prev) => !prev);
+  const onUserIdChanged = (e) => setUserId(e.target.value);
+
+  const canSave = [title, text, userId].every(Boolean) && !isLoading;
+
+  // Function to handle the save button click event
+  const onSaveNoteClicked = async (e) => {
+    if (canSave) {
+      await updateNote({ id: note.id, user: userId, title, text, completed });
+    }
+  };
+
+  // Function to handle the delete button click event
+  const onDeleteNoteClicked = async () => {
+    await deleteNote({ id: note.id });
+  };
+
+  // Format created and updated dates
+  const created = new Date(note.createdAt).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+  // Format updated date
+  const updated = new Date(note.updatedAt).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  // Generate options for the users select dropdown
+  const options = users.map((user) => {
+    return (
+      <option key={user.id} value={user.id}>
+        {" "}
+        {user.username}
+      </option>
+    );
+  });
+
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
+  const validTitleClass = !title ? "form__input--incomplete" : "";
+  const validTextClass = !text ? "form__input--incomplete" : "";
+
+  const errContent = (error?.data?.message || delerror?.data?.message) ?? "";
+
+  // Render the EditNoteForm component
+  const content = (
+    <>
+      <p className={errClass}>{errContent}</p>
+
+      <form className="form" onSubmit={(e) => e.preventDefault()}>
+        <div className="form__title-row">
+          <h2>Edit Note #{note.ticket}</h2> {/* Title of the form */}
+          <div className="form__action-buttons">
+            <button
+              className="icon-button"
+              title="Save"
+              onClick={onSaveNoteClicked}
+              disabled={!canSave}
+            >
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+            <button
+              className="icon-button"
+              title="Delete"
+              onClick={onDeleteNoteClicked}
+            >
+              <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="note-title">
+          {" "}
+          {/* Label for the title input */}
+          Title:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="note-title"
+          name="title"
+          type="text"
+          autoComplete="off"
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label className="form__label" htmlFor="note-text">
+          {" "}
+          {/* Label for the text input */}
+          Text:
+        </label>
+        <textarea
+          className={`form__input form__input--text ${validTextClass}`}
+          id="note-text"
+          name="text"
+          value={text}
+          onChange={onTextChanged}
+        />
+        <div className="form__row">
+          <div className="form__divider">
+            <label
+              className="form__label form__checkbox-container"
+              htmlFor="note-completed"
+            >
+              {" "}
+              {/* Label for the completed checkbox */}
+              WORK COMPLETE:
+              <input
+                className="form__checkbox"
+                id="note-completed"
+                name="completed"
+                type="checkbox"
+                checked={completed}
+                onChange={onCompletedChanged}
+              />
+            </label>
+
+            <label
+              className="form__label form__checkbox-container"
+              htmlFor="note-username"
+            >
+              {" "}
+              {/* Label for the user select dropdown */}
+              ASSIGNED TO:
+            </label>
+            <select
+              id="note-username"
+              name="username"
+              className="form__select"
+              value={userId}
+              onChange={onUserIdChanged}
+            >
+              {options}
+            </select>
+          </div>
+          <div className="form__divider">
+            <p className="form__created">
+              {" "}
+              {/* Display created date */}
+              Created:
+              <br />
+              {created}
+            </p>
+            <p className="form__updated">
+              {" "}
+              {/* Display updated date */}
+              Updated:
+              <br />
+              {updated}
+            </p>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+
+  return content;
+};
+
+export default EditNoteForm;
