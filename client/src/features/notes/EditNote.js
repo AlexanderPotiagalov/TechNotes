54add196bda71a6e3354ee6ba85a1da56c9a6109
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-29 18:17:30 -0700

    Updated NewNote.js to fetch users and render NewNoteForm and EditNote.js to retrieve note and users and render EditNoteForm.

--- client/src/features/notes/EditNote.js
+++ client/src/features/notes/EditNote.js
@@ -1,9 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectNoteById } from "./notesApiSlice";
+import { selectAllUsers } from "../users/UsersApiSlice";
+import EditNoteForm from "./EditNoteForm";
+
+// EditNote component to handle the editing of a note
 const EditNote = () => {
-  return (
-    <div>
-      <h1>Edit Note</h1>
-      {/* Add your edit note form or components here */}
-    </div>
-  );
+  const { id } = useParams();
+  const note = useSelector((state) => selectNoteById(state, id));
+  const users = useSelector(selectAllUsers);
+
+  // If the note and users are not yet loaded, show a loading message
+  const content =
+    note && users ? (
+      <EditNoteForm note={note} users={users} />
+    ) : (
+      <p>Loading...</p>
+    );
+
+  return content;
 };
 export default EditNote;
