91dca0296db79af942943b13f0124949ff43b70f
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-30 15:32:19 -0700

    Enhanced baseQuery with JWT support and automatic token refresh.

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,12 +1,55 @@
 // API Slice in Redux Toolkit is designed to simplify data flow, optimize caching, and streamline API requests. It automatically fetches and stores data efficiently, reducing unnecessary network calls and improving app performance.
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials } from "../../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  // Set the base URL for API requests
+  baseUrl: "http://localhost:3000",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  // console.log(args) // request url, method, body
+  // console.log(api) // signal, dispatch, getState()
+  // console.log(extraOptions) //custom like {shout: true}
+
+  let result = await baseQuery(args, api, extraOptions);
+
+  // If you want, handle other status codes, too
+  if (result?.error?.status === 403) {
+    console.log("sending refresh token");
+
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+
+      // retry original query with new access token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        refreshResult.error.data.message = "Your login has expired. ";
+      }
+      return refreshResult;
+    }
+  }
+
+  return result;
+};
 
 export const apiSlice = createApi({
   // Create an API slice using RTK Query
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
-    credentials: "include",
-  }), // Set the base URL for API requests
+  baseQuery: baseQueryWithReauth,
   tagTypes: ["Note", "User"], // Define tag types for cache management
   endpoints: (builder) => ({}), // Define endpoints using the builder pattern
 });
