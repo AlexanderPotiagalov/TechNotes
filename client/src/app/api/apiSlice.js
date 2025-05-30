9dc38a45253a4bc4a5eab44c21df3bb5aece5aa7
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-30 14:31:26 -0700

    Registerd auth reducer and updated base apiSlice.

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -3,7 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   // Create an API slice using RTK Query
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }), // Set the base URL for API requests
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000",
+    credentials: "include",
+  }), // Set the base URL for API requests
   tagTypes: ["Note", "User"], // Define tag types for cache management
   endpoints: (builder) => ({}), // Define endpoints using the builder pattern
 });
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer, // Register the API slice reducer
+    auth: authReducer, // Register the auth slice reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware), // Add the API slice middleware
