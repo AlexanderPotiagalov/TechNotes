9667a138dac66d94fd8addd021c4f97a01ba25e3
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-29 18:22:10 -0700

    Added Prefetch component to App.js for data initialization.

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import EditUser from "./features/users/EditUser";
 import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
+import Prefetch from "./features/auth/Prefetch";
 
 function App() {
   return (
@@ -17,21 +18,23 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-            <Route path="new" element={<NewNote />} />
-            <Route path=":id" element={<EditNote />} />
-          </Route>
+        <Route path="/*" element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path="new" element={<NewNote />} />
+              <Route path=":id" element={<EditNote />} />
+            </Route>
 
-          <Route path="users">
-            <Route index element={<UsersList />} />
-            <Route path="new" element={<NewUserForm />} />
-            <Route path=":id" element={<EditUser />} />
-          </Route>
-        </Route>{" "}
-        {/* End Dash */}
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path="new" element={<NewUserForm />} />
+              <Route path=":id" element={<EditUser />} />
+            </Route>
+          </Route>{" "}
+          {/* End Dash */}
+        </Route>
       </Route>
     </Routes>
   );
