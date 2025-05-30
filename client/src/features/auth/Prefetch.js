16d481b4d242fe27b08ebd923db3102524e40ec0
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-29 17:52:20 -0700

    Added Prefetch component to preload users and notes data with cleanup.

---
+++ client/src/features/auth/Prefetch.js
@@ -0,0 +1,28 @@
+// This component fetches users and notes before they're needed to speed up your app, and unsubscribes when the component unmounts to clean up memory.
+
+import { store } from "../../app/store";
+import { notesApiSlice } from "../notes/notesApiSlice";
+import { usersApiSlice } from "../users/UsersApiSlice";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
+// Prefetch component to initiate API calls for notes and users
+const Prefetch = () => {
+  // This component is used to prefetch data for notes and users when the application starts
+  useEffect(() => {
+    console.log("subscribing");
+    const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate());
+    const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+
+    // Cleanup function to unsubscribe from the API calls
+    return () => {
+      console.log("unsubscribing");
+      notes.unsubscribe();
+      users.unsubscribe();
+    };
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+  return <Outlet />; // Render the Outlet component to allow nested routes to render their components
+};
+
+export default Prefetch;
