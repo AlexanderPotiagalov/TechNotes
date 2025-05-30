8387972f4814b4e1ada5651c4b741ab1cb05208d
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-29 17:41:34 -0700

    Updated EditUser and NewUserForm components, add new EditUserForm component for betting user management.

--- client/src/features/users/EditUser.js
+++ client/src/features/users/EditUser.js
@@ -1,9 +1,13 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUserById } from "./UsersApiSlice";
+import EditUserForm from "./EditUserForm";
+
+// EditUser component to handle the editing of a user
 const EditUser = () => {
-  return (
-    <div>
-      <h1>Edit User</h1>
-      {/* Add form or components to edit user details here */}
-    </div>
-  );
+  const { id } = useParams();
+  const user = useSelector((state) => selectUserById(state, id));
+  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
+  return content;
 };
 export default EditUser;
--- client/src/features/users/EditUserForm.js
+++ client/src/features/users/EditUserForm.js
@@ -1,43 +1,52 @@
 import { useState, useEffect } from "react";
-import { useAddNewUserMutation } from "./UsersApiSlice";
+import { useUpdateUserMutation, useDeleteUserMutation } from "./UsersApiSlice";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
 
 const USER_REGEX = /^[A-z]{3,20}$/; // Username must be 3-20 letters
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/; // Password must be 4-12 characters including letters, numbers, and special characters
 
-// NewUserForm component for creating a new user
-const NewUserForm = () => {
-  const [addNewUser, { isLoading, isSuccess, isError, error }] =
-    useAddNewUserMutation();
+// EditUserForm component for editing an existing user
+const EditUserForm = ({ user }) => {
+  // Mutation hooks for updating a user
+  const [updateUser, { isLoading, isSuccess, isError, error }] =
+    useUpdateUserMutation();
+
+  // Mutation hooks for deleting a user
+  const [
+    deleteUser,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteUserMutation();
 
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState(user.username);
   const [validUsername, setValidUsername] = useState(false);
   const [password, setPassword] = useState("");
   const [validPassword, setValidPassword] = useState(false);
-  const [roles, setRoles] = useState(["Employee"]);
+  const [roles, setRoles] = useState(user.roles);
+  const [active, setActive] = useState(user.active);
 
   // Validate username and password using regex
   useEffect(() => {
     setValidUsername(USER_REGEX.test(username));
   }, [username]);
-
   useEffect(() => {
     setValidPassword(PWD_REGEX.test(password));
   }, [password]);
 
+  // Reset form fields and navigate to users list on successful update or delete
   useEffect(() => {
-    if (isSuccess) {
+    console.log(isSuccess);
+    if (isSuccess || isDelSuccess) {
       setUsername("");
       setPassword("");
       setRoles([]);
       navigate("/dash/users");
     }
-  }, [isSuccess, navigate]);
+  }, [isSuccess, isDelSuccess, navigate]);
 
   const onUsernameChanged = (e) => setUsername(e.target.value); // Update username state on input change
   const onPasswordChanged = (e) => setPassword(e.target.value); // Update password state on input change
@@ -45,22 +54,27 @@ const NewUserForm = () => {
   // Update roles state when roles are changed in the select dropdown
   const onRolesChanged = (e) => {
     const values = Array.from(
-      e.target.selectedOptions, // HTMLCollection
+      e.target.selectedOptions,
       (option) => option.value
     );
     setRoles(values);
   };
 
-  // Check if the form can be saved based on the validity of username, password, roles, and loading state
-  const canSave =
-    [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+  // Toggle active state when checkbox is changed
+  const onActiveChanged = () => setActive((prev) => !prev);
 
-  // Function to handle the save button click event
+  // Check if the form can be saved based on the validity of username, password, roles, and loading state
   const onSaveUserClicked = async (e) => {
-    e.preventDefault();
-    if (canSave) {
-      await addNewUser({ username, password, roles });
+    if (password) {
+      await updateUser({ id: user.id, username, password, roles, active });
+    } else {
+      await updateUser({ id: user.id, username, roles, active });
     }
+  };
+
+  // Function to handle the delete button click event
+  const onDeleteUserClicked = async () => {
+    await deleteUser({ id: user.id });
   };
 
   // Generate options for the roles select dropdown
@@ -73,25 +87,49 @@ const NewUserForm = () => {
     );
   });
 
+  // Determine if the form can be saved based on the validity of inputs and loading state
+  let canSave;
+  if (password) {
+    canSave =
+      [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+  } else {
+    canSave = [roles.length, validUsername].every(Boolean) && !isLoading;
+  }
+
   // Determine the error class based on whether there is an error
-  const errClass = isError ? "errmsg" : "offscreen";
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
   const validUserClass = !validUsername ? "form__input--incomplete" : "";
-  const validPwdClass = !validPassword ? "form__input--incomplete" : "";
+  const validPwdClass =
+    password && !validPassword ? "form__input--incomplete" : "";
   const validRolesClass = !Boolean(roles.length)
     ? "form__input--incomplete"
     : "";
 
-  // Render the NewUserForm component
+  const errContent = (error?.data?.message || delerror?.data?.message) ?? "";
+
+  // Render the EditUserForm component
   const content = (
     <>
-      <p className={errClass}>{error?.data?.message}</p>
+      <p className={errClass}>{errContent}</p>
 
-      <form className="form" onSubmit={onSaveUserClicked}>
+      <form className="form" onSubmit={(e) => e.preventDefault()}>
         <div className="form__title-row">
-          <h2>New User</h2> {/* Title of the form */}
+          <h2>Edit User</h2> {/* Title of the form */}
           <div className="form__action-buttons">
-            <button className="icon-button" title="Save" disabled={!canSave}>
+            <button
+              className="icon-button"
+              title="Save"
+              onClick={onSaveUserClicked}
+              disabled={!canSave}
+            >
               <FontAwesomeIcon icon={faSave} />
+            </button>
+            <button
+              className="icon-button"
+              title="Delete"
+              onClick={onDeleteUserClicked}
+            >
+              <FontAwesomeIcon icon={faTrashCan} />
             </button>
           </div>
         </div>
@@ -110,8 +148,9 @@ const NewUserForm = () => {
         />
 
         <label className="form__label" htmlFor="password">
-          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>{" "}
+          Password: <span className="nowrap">[empty = no change]</span>{" "}
           {/* Label for the password input */}
+          <span className="nowrap">[4-12 chars incl. !@#$%]</span>
         </label>
         <input
           className={`form__input ${validPwdClass}`}
@@ -122,8 +161,26 @@ const NewUserForm = () => {
           onChange={onPasswordChanged}
         />
 
-        {/* Label for the roles select dropdown */}
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="user-active"
+        >
+          {" "}
+          {/* Label for the active checkbox */}
+          ACTIVE:
+          <input
+            className="form__checkbox"
+            id="user-active"
+            name="user-active"
+            type="checkbox"
+            checked={active}
+            onChange={onActiveChanged}
+          />
+        </label>
+
         <label className="form__label" htmlFor="roles">
+          {" "}
+          {/* Label for the roles select dropdown */}
           ASSIGNED ROLES:
         </label>
         <select
@@ -143,4 +200,4 @@ const NewUserForm = () => {
 
   return content;
 };
-export default NewUserForm;
+export default EditUserForm;
