5bc10c0bdca8b4e29f82e9d17a741ec6ea06adff
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-30 15:38:31 -0700

    Removed useless useEffect hook from DashHeader.

--- client/src/components/DashHeader.js
+++ client/src/components/DashHeader.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, Link, useLocation } from "react-router-dom";
