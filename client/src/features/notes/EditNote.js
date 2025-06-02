cafe0b505ba2e5838e93b7d4d613bd936f5bd528
AlexanderPotiagalov <apa168@sfu.ca>
2025-06-02 16:11:20 -0700

    Updated Note, EditNote, and NewNote for consistent user resolution.

--- client/src/features/notes/EditNote.js
+++ client/src/features/notes/EditNote.js
@@ -1,22 +1,38 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectNoteById } from "./notesApiSlice";
-import { selectAllUsers } from "../users/UsersApiSlice";
 import EditNoteForm from "./EditNoteForm";
+import { useGetNotesQuery } from "./notesApiSlice";
+import { useGetUsersQuery } from "../users/UsersApiSlice";
+import useAuth from "../../hooks/useAuth";
+import PulseLoader from "react-spinners/PulseLoader";
+import useTitle from "../../hooks/useTitle";
 
 // EditNote component to handle the editing of a note
 const EditNote = () => {
+  useTitle("techNotes: Edit Note");
   const { id } = useParams();
-  const note = useSelector((state) => selectNoteById(state, id));
-  const users = useSelector(selectAllUsers);
+  const { username, isManager, isAdmin } = useAuth();
+  const { note } = useGetNotesQuery("notesList", {
+    selectFromResult: ({ data }) => ({
+      note: data?.entities[id],
+    }),
+  });
+
+  const { users } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      users: data?.ids.map((id) => data?.entities[id]),
+    }),
+  });
+
+  if (!note || !users?.length) return <PulseLoader color={"#FFF"} />;
+
+  if (!isManager && !isAdmin) {
+    if (note.username !== username) {
+      return <p className="errmsg">No access</p>;
+    }
+  }
 
   // If the note and users are not yet loaded, show a loading message
-  const content =
-    note && users ? (
-      <EditNoteForm note={note} users={users} />
-    ) : (
-      <p>Loading...</p>
-    );
+  const content = <EditNoteForm note={note} users={users} />;
 
   return content;
 };
