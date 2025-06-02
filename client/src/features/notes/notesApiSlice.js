0c4f2ebbb3260219f99acb40824288e82892a495
AlexanderPotiagalov <apa168@sfu.ca>
2025-06-02 16:10:20 -0700

    Normalized notes with username field in notesApiSlice.

--- client/src/features/notes/notesApiSlice.js
+++ client/src/features/notes/notesApiSlice.js
@@ -3,33 +3,34 @@ import { apiSlice } from "../../app/api/apiSlice";
 
 const notesAdapter = createEntityAdapter({
   sortComparer: (a, b) =>
-    a.completed === b.completed ? 0 : a.completed ? 1 : -1, // Sort notes by completion status
-}); // Create an entity adapter for notes
-const initialState = notesAdapter.getInitialState(); // Get the initial state for notes
+    a.completed === b.completed ? 0 : a.completed ? 1 : -1,
+});
+
+const initialState = notesAdapter.getInitialState();
 
 export const notesApiSlice = apiSlice.injectEndpoints({
-  // Inject endpoints into the API slice
   endpoints: (builder) => ({
     getNotes: builder.query({
-      // Define a query to get notes
-      query: () => "/notes", // Set the endpoint to fetch notes
-      validateStatus: (response, result) => {
-        return response.status === 200 && !result.isError; // Validate the response status
-      },
+      query: () => ({
+        url: "/notes",
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
       transformResponse: (responseData) => {
         const loadedNotes = responseData.map((note) => {
-          note.id = note._id; // Map the _id field to id for consistency
+          note.id = note._id;
           return note;
         });
-        return notesAdapter.setAll(initialState, loadedNotes); // Set all notes in the state
+        return notesAdapter.setAll(initialState, loadedNotes);
       },
       providesTags: (result, error, arg) => {
         if (result?.ids) {
           return [
-            { type: "Note", id: "LIST" }, // Provide a tag for the list of notes
-            ...result.ids.map((id) => ({ type: "Note", id })), // Provide tags for each note
+            { type: "Note", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "Note", id })),
           ];
-        } else return [{ type: "Note", id: "LIST" }]; // Fallback tag if no notes are found
+        } else return [{ type: "Note", id: "LIST" }];
       },
     }),
     addNewNote: builder.mutation({
@@ -68,19 +69,23 @@ export const {
   useAddNewNoteMutation,
   useUpdateNoteMutation,
   useDeleteNoteMutation,
-} = notesApiSlice; // Export the hook to use the getNotes query
+} = notesApiSlice;
 
-export const selectNotesResult = notesApiSlice.endpoints.getNotes.select(); // Select the result of the getNotes query
-// create memoized selector to get the notes result
-export const selectNotesData = createSelector(
+// returns the query result object
+export const selectNotesResult = notesApiSlice.endpoints.getNotes.select();
+
+// creates memoized selector
+const selectNotesData = createSelector(
   selectNotesResult,
-  (notesResult) => notesResult.data // Create a selector to get the notes data
+  (notesResult) => notesResult.data // normalized state object with ids & entities
 );
 
+//getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
   selectAll: selectAllNotes,
   selectById: selectNoteById,
   selectIds: selectNoteIds,
+  // Pass in a selector that returns the notes slice of state
 } = notesAdapter.getSelectors(
   (state) => selectNotesData(state) ?? initialState
-); // Get selectors for notes data
+);
