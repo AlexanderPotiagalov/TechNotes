32cb37be3f5ce4c7c43d4362dbe7bf285b16a37a
AlexanderPotiagalov <apa168@sfu.ca>
2025-06-02 17:16:32 -0700

    Removed trailing / at the end of the url to properly deploy.

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   // Define an array of allowed origins for CORS
-  "https://technoteshub-dis4.onrender.com/",
+  "https://technoteshub-dis4.onrender.com",
 ];
 
 module.exports = allowedOrigins; // export the allowed origins array for use in other modules
