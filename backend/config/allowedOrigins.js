bd26bf52cc17d552f570a19b9e59479364c5276a
AlexanderPotiagalov <apa168@sfu.ca>
2025-06-02 17:01:14 -0700

    Removed unallowed url origins for CORS in allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,9 +1,6 @@
 const allowedOrigins = [
   // Define an array of allowed origins for CORS
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "https://www.example.com",
-  "https://example.com",
+  "https://technoteshub-dis4.onrender.com/",
 ];
 
 module.exports = allowedOrigins; // export the allowed origins array for use in other modules
